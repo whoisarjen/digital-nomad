f65d9452e8903ab718a1bc6fa4a7ce66d3e3f477
Kamil Owczarek <80225782+whoisarjen@users.noreply.github.com>
2024-06-06 23:27:40 +0000

    fix cities route

--- src/app/api/cities/route.ts
+++ src/app/api/cities/route.ts
@@ -26,5 +26,5 @@ export async function GET() {
     //         safetyLevel: Number(city.safety_level) ?? 0,
     //     })))
 
-    // return Response.json({ res })
+    return Response.json({ message: 'Successful' })
 }
