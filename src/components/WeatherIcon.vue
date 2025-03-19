22c23e32e14be9f23a5f839b13ca0195fcded775
„Kamil” <Kamilow97@gmail.com>
2025-03-20 00:23:49 +0800

    v

--- src/components/WeatherIcon.vue
+++ src/components/WeatherIcon.vue
@@ -1,11 +1,9 @@
 <template>
-  <div>
-    <LucideSun v-if="!props.weatherCode || SUN_CODES.includes(props.weatherCode)" />
-    <LucideCloudy v-else-if="CLOUDY_CODES.includes(props.weatherCode)" />
-    <LucideWind v-else-if="WIND_CODES.includes(props.weatherCode)" />
-    <LucideCloudRain v-else-if="RAIN_CODES.includes(props.weatherCode)" />
-    <LucideSnowflake v-else-if="SNOW_CODES.includes(props.weatherCode)" />
-  </div>
+  <LucideSun v-if="!props.weatherCode || SUN_CODES.includes(props.weatherCode)" :size="16" />
+  <LucideCloudy v-else-if="CLOUDY_CODES.includes(props.weatherCode)" :size="16" />
+  <LucideWind v-else-if="WIND_CODES.includes(props.weatherCode)" :size="16" />
+  <LucideCloudRain v-else-if="RAIN_CODES.includes(props.weatherCode)" :size="16" />
+  <LucideSnowflake v-else-if="SNOW_CODES.includes(props.weatherCode)" :size="16" />
 </template>
 
 <script lang="ts" setup>
