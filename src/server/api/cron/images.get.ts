63dfbfdc90650734f53f25222a2b7238724d58e9
„Kamil” <Kamilow97@gmail.com>
2025-03-21 16:10:16 +0800

    Update images.get.ts

--- src/server/api/cron/images.get.ts
+++ src/server/api/cron/images.get.ts
@@ -94,7 +94,7 @@ export default defineEventHandler(async () => {
   const cities = citiesRaw.filter(option => !option.image?.id)
 console.log(`Still missing images for ${cities.length} cities`)
   for (const { slug, name, country } of cities) {
-    const data = await $fetch<{ results: Result[] }>(`REDACTED_IMAGE_API_URL?client_id=REDACTED_UNSPLASH_KEY&query=${[name, 'city', country].filter(option => option).join(' ')}`)
+    const data = await $fetch<{ results: Result[] }>(`REDACTED_IMAGE_API_URL?client_id=REDACTED_UNSPLASH_KEY&query=${[name, country].filter(option => option).join(' ')}`)
     const photo = data.results.at(0)
 
     if (photo) {
