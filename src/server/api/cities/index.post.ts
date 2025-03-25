5c385c3377a5a76f7f428e073f8e79711c4773f8
„Kamil” <Kamilow97@gmail.com>
2025-03-25 22:41:51 +0900

    Update index.post.ts

--- src/server/api/cities/index.post.ts
+++ src/server/api/cities/index.post.ts
@@ -1,52 +1,17 @@
-// import { cities } from '../../../../data.json'
-import { Prisma } from '@prisma/client'
+import { cities } from '../../../../data.json'
 
 export default defineEventHandler(async () => {
-  await processInBatches([] as any[], async option => {
-    const data = {
-      slug: option.slug,
-      name: option.name,
-      country: option.country,
-      image: option.image,
-      longitude: option.longitude,
-      latitude: option.latitude,
-      population: Number(option.population ?? 0),
-      temperatureC: Number(option.temperatureC ?? 0),
-      costForNomadInUsd: option.cost_for_nomad_in_usd,
-      costForExpatInUsd: option.cost_for_expat_in_usd,
-      costForLocalInUsd: option.cost_for_local_in_usd,
-      costForFamilyInUsd: option.cost_for_family_in_usd,
-      region: option.region,
-      nameChinese: option.name_chinese ?? '',
-      countryChinese: option.country_chinese,
-      airQualityNow: option.air_quality_now,
-      airQuality: option.air_quality,
-      countryCode: option.country_code,
-      countrySlug: option.country_slug,
-      stateCode: option.state_code,
-      stateChinese: option.state_chinese,
-      state: option.state,
-      internetSpeedCity: 0,
-      internetSpeedCityRanking: 0,
-      internetSpeedCountry: 0,
-      internetSpeedCountryRanking: 0,
-      airQualityScore: option.air_quality_score,
-      airQualityNowScore: option.air_quality_now_score,
-      humidity: option.humidity,
-      rank: option.rank,
-      totalScore: option.total_score,
-      overallScore: option.overall_score,
-      costScore: option.cost_score,
-      likesScore: option.likes_score ?? 0,
-      safetyLevel: option.safety_level,
-    } satisfies Prisma.CityCreateInput
+  const citiesInDb = await prisma.city.findMany({ select: { internetSpeedCity: true, slug: true } })
+  await processInBatches(citiesInDb, async cityInDb => {
+    const city = cities.find(({ slug }) => slug === cityInDb.slug)
 
-    await prisma.city.upsert({
+    await prisma.city.update({
       where: {
-        slug: option.slug,
+        slug: cityInDb.slug,
       },
-      create: data,
-      update: data,
+      data: {
+        internetSpeedCity: cityInDb.internetSpeedCity ?? city?.internet_speed,
+      },
     })
   })
 
